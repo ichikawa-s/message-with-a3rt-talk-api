0e2c00d74f4a09492d113cf982175680c5c11faf
ichikawa satoshi <bigbridge259@gmail.com>
2018-04-08 15:44:34 +0900

    新規作成。

---
+++ js/message.js
@@ -0,0 +1,66 @@
+/**
+ * 送信ボタン押下時にタイムラインにメッセージを表示する
+ */
+function displayMessageTo() {
+	// $().click()
+	
+	var message = getMessage();
+	
+	console.log( message );
+
+	appendMessageToTimeline( message );
+	
+}
+
+function getMessage() {
+	return $('#bms_send_message').val();
+}
+
+function appendMessageToTimeline( message ) {
+	$('.messages-wrapper').append( '<div class="message to">' + message + '</div>' );
+}
+
+function appendMessageFromTimeline( message ) {
+	$('.messages-wrapper').append( '<div class="message from">' + message + '</div>' );
+}
+
+$('#bms_send_btn').on('click', function(){
+	// タイムラインに送信メッセージを表示する
+	displayMessageTo();
+	
+	// talk apiに送信する
+	$.ajax({
+		  type: "POST",
+		  url: "https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk",
+		  data: { apikey: A3RT_API_SETTING.apikey,
+			      query: getMessage }
+		  ,
+		  success: function( data ) {
+			  console.log( data.status );
+			  console.log( data.message );
+			  console.log( data.results );
+			  
+			  displayMessageFrom( data.results );
+
+			  // 送信メッセージをクリア
+		　　　　　$('#bms_send_message').val('');
+
+		  }
+		  /*
+		  ,
+		  dataType: dataType
+		  */
+		});
+});
+
+function displayMessageFrom( results ) {
+	
+	var message = results[0].reply;
+	
+	if ( message ) {
+		appendMessageFromTimeline( message );
+	} else {
+		console.log( 'data.results = ' + results );
+	}
+	
+}
