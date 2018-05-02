d2631c9976e607d8f0ec5d55561dd166cf6b7cc0
ichikawa satoshi <bigbridge259@gmail.com>
2018-05-02 17:36:26 +0900

    index.jsを簡略化。

--- request-talkapi-as-node-app/index.js
+++ request-talkapi-as-node-app/index.js
@@ -2,21 +2,6 @@
 var express = require("express");
 var app = express();
 
-/* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
-/*
-var server = app.listen(3000, function(){
-    console.log("Node.js is listening to PORT:" + server.address().port);
-});
-*/
-
-/* 3. GETアクセス時に呼ばれる */
-/*
-app.get("/", function(req, res, next){
-    console.log("Http access now");
-});
-*/
-
-
 /* 4. Node.jsからAPIエンドポイントにHTTPアクセスを飛ばす */
 //requestをrequire
 var request = require('request');
@@ -37,37 +22,32 @@ var options = {
          }
 }
 
-//リクエスト送信
-/*
-request(options, function (error, response, body) {
-  //コールバックで色々な処理
-  console.log( response );
-})
-*/
-
+// talk apiにリクエストを送信する関数
 var requestToTalkApi = function (req, res, next) {
+
+	console.log('http access recieved !');
+	
 	//talk apiにリクエスト送信
 	request(options, function (error, response, body) {
-	 	//コールバックで色々な処理
-	 	
-	 	// console.log(response);
-	 	// res.data = response;
-	 	res.talkapiresult = response; // expressのresのプロパティにrequestモジュールのアクセス結果を保持する
-	 	next(); // next()はrequestのcallbackのなかでOK？
+		//コールバックで色々な処理
+		
+		// console.log(response);
+		// res.data = response;
+		// res.locals.talkapiresult = response; // expressのresのプロパティにrequestモジュールのアクセス結果を保持する
+		// next(); // 次の処理に進む
+	
+		var responseText = response.body;
+
+		// 応答データをJSON形式で送信します
+		res.json(responseText);
 	})
 
 };
 
-app.use(requestToTalkApi);
+// http://192.168.56.4:3000/ にアクセスしたときに呼ばれるroute メソッド
+app.get('/', requestToTalkApi);
 
-app.get('/', function (req, res) {
-  console.log("Http access now");
-  var responseText = res.talkapiresult.body;
-  // res.send(responseText);
-  // JSON 応答を送信します
-  res.json(responseText);
-});
-
+// ポート番号3000番で待ち受ける
 var server = app.listen(3000, function(){
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
