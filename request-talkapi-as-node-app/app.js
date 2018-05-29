879d5503d659a6807b98f346e79e1325bc4aa406
ichikawa-s <ichikawa-s@sgnet.co.jp>
2018-05-29 13:30:17 +0900

    post to express, and receive json data by express.

--- request-talkapi-as-node-app/app.js
+++ request-talkapi-as-node-app/app.js
@@ -2,6 +2,13 @@
 var express = require("express");
 var app = express();
 
+// POSTされたデータを取り扱うためのモジュール
+var bodyParser = require('body-parser')
+
+// parse application/json
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}));
+
 /* 4. Node.jsからAPIエンドポイントにHTTPアクセスを飛ばす */
 //requestをrequire
 var request = require('request');
@@ -17,25 +24,36 @@ var options = {
   method: 'POST',
   headers: headers,
   json: true,
+  /*
   form: { "apikey" : "s9DxJCNnRgmzJBprDUUZrPLXgaY3dcEU",
           "query"  : "おはよう"
          }
+   */
+   form: { "apikey" : "s9DxJCNnRgmzJBprDUUZrPLXgaY3dcEU" }
 }
 
 // talk apiにリクエストを送信する関数
 var requestToTalkApi = function (req, res, next) {
 
 	console.log('http access recieved !');
-	
+  console.log('req.body = ' + JSON.stringify(req.body) );
+
+  // パラメータ名、messageを出力
+  console.log("Recieved message is " + req.body.message);
+  var message = req.body.message;
+  // console.log("req.body is " + req.body);
+
+  options.form.query = message;
+
 	//talk apiにリクエスト送信
 	request(options, function (error, response, body) {
 		//コールバックで色々な処理
-		
+
 		// console.log(response);
 		// res.data = response;
 		// res.locals.talkapiresult = response; // expressのresのプロパティにrequestモジュールのアクセス結果を保持する
 		// next(); // 次の処理に進む
-	
+
 		var responseText = response.body;
 
 		// 応答データをJSON形式で送信します
@@ -45,7 +63,8 @@ var requestToTalkApi = function (req, res, next) {
 };
 
 // http://192.168.56.4:3000/ にアクセスしたときに呼ばれるroute メソッド
-app.get('/requestToTalkApi', requestToTalkApi);
+// app.get('/requestToTalkApi', requestToTalkApi);
+app.post('/requestToTalkApi', requestToTalkApi);
 
 // ポート番号3000番で待ち受ける
 var server = app.listen(3000, function(){
@@ -55,4 +74,3 @@ var server = app.listen(3000, function(){
 
 // publicフォルダ内の静的ファイルのHTTPアクセスを許可する
 app.use('/', express.static('public'));
-
--- request-talkapi-as-node-app/public/js/message.js
+++ request-talkapi-as-node-app/public/js/message.js
@@ -3,13 +3,13 @@
  */
 function displayMessageTo() {
 	// $().click()
-	
+
 	var message = getMessage();
-	
+
 	console.log( message );
 
 	appendMessageToTimeline( message );
-	
+
 }
 
 function getMessage() {
@@ -27,8 +27,9 @@ function appendMessageFromTimeline( message ) {
 $('#bms_send_btn').on('click', function(){
 	// タイムラインに送信メッセージを表示する
 	displayMessageTo();
-	
+
 	// talk apiに送信する
+	/*
 	$.ajax({
 		  type: "POST",
 		  url: "https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk",
@@ -39,28 +40,47 @@ $('#bms_send_btn').on('click', function(){
 			  console.log( data.status );
 			  console.log( data.message );
 			  console.log( data.results );
-			  
+
 			  displayMessageFrom( data.results );
 
 			  // 送信メッセージをクリア
 		　　　　　$('#bms_send_message').val('');
 
 		  }
-		  /*
-		  ,
-		  dataType: dataType
-		  */
 		});
+		*/
+
+		var data_for_send = JSON.stringify( { 'message' : getMessage() } );
+		console.log( 'data = ' + data_for_send );
+
+		$.ajax({
+			  type: "POST",
+			  url: "http://localhost:3000/requestToTalkApi",
+			  data: JSON.parse( data_for_send ),
+				dataType:'json',
+				// contentType: 'application/json',
+			  success: function( data ) {
+				  console.log( data.status );
+				  console.log( data.message );
+				  console.log( data.results );
+
+				  displayMessageFrom( data.results );
+
+				  // 送信メッセージをクリア
+			　　　　　$('#bms_send_message').val('');
+
+			  }
+			});
 });
 
 function displayMessageFrom( results ) {
-	
+
 	var message = results[0].reply;
-	
+
 	if ( message ) {
 		appendMessageFromTimeline( message );
 	} else {
 		console.log( 'data.results = ' + results );
 	}
-	
+
 }
